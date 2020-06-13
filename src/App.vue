c7e88cb4bb09261860f5ec7b89577d3b531d8cd8
P.Jeffrey <peterk.jeffrey@sa.gov.au>
2020-06-13 14:58:06 +0930

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="App">
+    <header class="App-header">
+      <img src="favicon.png" alt="logo" title="Forkify in Vue">
+      <h1>Forkify in Vue</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+.App {
+  margin: 1em 2%;
+}
+.App-header {
+  display: flex;
+  align-items: center;
+}
+.App-header img {
+  width: 50px;
+  height: 50px;
+  margin-right: 1em;
+}
+</style>
